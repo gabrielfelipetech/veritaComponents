fd55b7c1995af61348d3da1e17751ad820b9fc01
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2025-07-24 17:15:22 -0300

    feat: add input at the home

--- app.vue
+++ app.vue
@@ -1,28 +1,119 @@
 <template>
   <div class="space-y-8 p-6">
-
-    <section  class="space-y-4 p-4 border rounded-lg">
-      <VButton icon="mdi:check" iconPosition="left" >
-        Confirmar
-      </VButton>
-      <VButton icon="mdi:arrow-right" iconPosition="right">
-        Continuar
-      </VButton>
-      <VButton loading icon="line-md:loading-twotone-loop" >
-        Carregando
-      </VButton>
-      <VInput
-        label="E-mail"
-        placeholder="Digite seu e-mail"
-        type="email"
-        icon="mdi:email-outline"
-        iconPosition="left"
-        required
-        size="md"
-      />
+    <section class="p-4 border rounded-lg  ">
+      <h1>Button Light</h1>
+      <div class="flex gap-4">
+        <VButton size="sm"
+                 icon="mdi:check"
+                 iconPosition="left"
+                 color="primary-em">
+          Confirmar
+        </VButton>
+        <VButton size="md"
+                 icon="mdi:arrow-right"
+                 iconPosition="right"
+                 color="secondary-em">
+          Continuar
+        </VButton>
+        <VButton size="lg"
+                 icon="line-md:loading-twotone-loop"
+                 color="tertiary-em">
+          Carregando
+        </VButton>
+        <VButton loading
+                 size="lg"
+                 icon="line-md:loading-twotone-loop"
+                 color="tertiary-em">
+          Carregando
+        </VButton>
+      </div>
+      <h1>Button Dark</h1>
+      <div class="flex gap-4">
+        <VButton size="sm"
+                 icon="mdi:check"
+                 iconPosition="left"
+                 color="primary-em"
+                 :isThemeDark="true">
+          Confirmasr
+        </VButton>
+        <VButton size="md"
+                 icon="mdi:arrow-right"
+                 iconPosition="right"
+                 color="secondary-em"
+                 :isThemeDark="true">
+          Continuar
+        </VButton>
+        <VButton size="lg"
+                 icon="line-md:loading-twotone-loop"
+                 color="tertiary-em"
+                 :isThemeDark="true">
+          Carregando
+        </VButton>
+        <VButton loading
+                 size="lg"
+                 icon="line-md:loading-twotone-loop"
+                 color="tertiary-em"
+                 :isThemeDark="true">
+          Carregando
+        </VButton>
+      </div>
     </section>
+      <section class="p-4 border rounded-lg">
+      <h1>Input Light</h1>
+      <div class="flex gap-4">
+        <VInput
+          label="Nome"
+          placeholder="Digite seu nome"
+          icon="mdi:account"
+          iconPosition="left"
+          color="primary-em"
+        />
+        <VInput
+          label="E-mail"
+          placeholder="foo@bar.com"
+          type="email"
+          size="md"
+          color="secondary-em"
+        />
+        <VInput
+          label="Senha"
+          placeholder=""
+          type="text"
+          size="lg"
+          color="tertiary-em"
+          error="Mínimo 8 caracteres"
+        />
+      </div>
 
-
+      <h1>Input Dark</h1>
+      <div class="flex gap-4 bg-gray-800 p-4">
+        <VInput
+          label="Nome"
+          placeholder="Digite seu nome"
+          icon="mdi:account"
+          iconPosition="left"
+          color="primary-em"
+          :isThemeDark="true"
+        />
+        <VInput
+          label="E-mail"
+          placeholder="foo@bar.com"
+          type="email"
+          size="md"
+          color="secondary-em"
+          :isThemeDark="true"
+        />
+        <VInput
+          label="Senha"
+          placeholder="••••••••"
+          type="password"
+          size="md"
+          color="tertiary-em"
+          error="Mínimo 8 caracteres"
+          :isThemeDark="true"
+        />
+      </div>
+    </section>
   </div>
 </template>
 
