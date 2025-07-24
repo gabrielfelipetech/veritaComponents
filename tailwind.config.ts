dfbe6e9371103422af7525d02f001af3a992bf66
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2025-07-24 17:16:03 -0300

    fix: adjust storybook to see tailwind v2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,15 @@ const config: Config = {
     './layouts/**/*.{vue,js,ts}',
     './pages/**/*.{vue,js,ts}',
     './app.vue',
-    './.storybook/**/*.{js,ts}',   
+    './.storybook/**/*.{js,ts,mdx}',
     './node_modules/verita-components/dist/**/*.{js,ts}'
+  ],
+  safelist: [
+    'h-8','text-sm','px-4',
+    'h-10','text-base','px-5',
+    'h-12','text-lg','px-6',
+    'w-full','w-auto',
+    'disabled:opacity-50','disabled:cursor-not-allowed'
   ],
   theme: {
     extend: {
@@ -16,7 +23,7 @@ const config: Config = {
         secondary: '#facc15'
       }
     }
-  },
+  }
 }
 
 export default config
