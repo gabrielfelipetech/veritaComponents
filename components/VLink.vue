066713d9cdf615a657f720263657c6c10742d6a8
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2025-07-28 14:09:31 -0300

    fix: adjust VLink

--- components/VLink.vue
+++ components/VLink.vue
@@ -1,48 +1,36 @@
 <template>
-  <component
-    :is="isExternal ? 'a' : NuxtLink"
-    v-bind="linkProps"
-    v-on="$attrs"
-  >
+  <component :is="isExternal ? 'a' : 'NuxtLink'"
+             v-bind="linkProps"
+             v-on="$attrs">
     <slot />
   </component>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
-import { NuxtLink } from '#components'
 
 const props = defineProps({
-  to:            { type: String, required: true },
-  external:      { type: Boolean, default: false },
-  replace:       { type: Boolean, default: false },
-  prefetch:      { type: Boolean, default: true },
-  target:        { type: String, default: undefined },
-  rel:           { type: String, default: undefined }
+  to: { type: String, required: true },
+  external: { type: Boolean, default: false },
+  replace: { type: Boolean, default: false },
+  prefetch: { type: Boolean, default: true },
+  target: { type: String, default: undefined },
+  rel: { type: String, default: undefined }
 })
 
-const isExternal = computed(() => {
-  if (props.external) return true
-  return /^(https?:|\/\/|mailto:|tel:|#)/.test(props.to)
-})
+const isExternal = computed(() =>
+  props.external ||
+  /^(https?:|\/\/|mailto:|tel:|#)/.test(props.to)
+)
 
 const computedRel = computed(() => {
   if (props.rel) return props.rel
   if (props.target === '_blank') return 'noopener noreferrer'
-  return undefined
 })
 
-const linkProps = computed(() => {
-  return isExternal.value
-    ? {
-        href:  props.to,
-        target: props.target,
-        rel:    computedRel.value
-      }
-    : {
-        to:      props.to,
-        replace: props.replace,
-        prefetch: props.prefetch
-      }
-})
+const linkProps = computed(() =>
+  isExternal.value
+    ? { href: props.to, target: props.target, rel: computedRel.value }
+    : { to: props.to, replace: props.replace, prefetch: props.prefetch }
+)
 </script>
