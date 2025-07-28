4c58e08067deaaf6a826a9af523468ca2e4a4e3e
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2025-07-28 14:13:10 -0300

    fix: adjust vquestiongroup

--- components/VQuestionGroup.vue
+++ components/VQuestionGroup.vue
@@ -4,16 +4,9 @@
     <div class="flex flex-col gap-2">
       <component
         v-for="opt in options"
-        :key="opt.value"
+        :key="String(opt.value)"
         :is="inputComponent"
-        v-model="modelValue"
-        :name="name"
-        :value="opt.value"
-        :labelText="opt.label"
-        :labelPosition="labelPosition"
-        :disabled="disabled"
-        size="md"
-        :color="color"
+        v-bind="getProps(opt)"
       />
     </div>
   </fieldset>
@@ -21,9 +14,9 @@
 
 <script setup lang="ts">
 import { computed, toRef } from 'vue'
-import VRadio from './VRadio.vue'
+import VRadio    from './VRadio.vue'
 import VCheckbox from './VCheckbox.vue'
-import VToggle from './VToggle.vue'
+import VToggle   from './VToggle.vue'
 
 interface Option {
   label: string
@@ -35,7 +28,7 @@ const props = defineProps<{
   name?: string
   options: Option[]
   type?: 'radio' | 'checkbox' | 'toggle'
-  modelValue?: string | boolean | Array<string | number | boolean>
+  modelValue?: string | boolean | Array<string|number|boolean>
   labelPosition?: 'left' | 'right'
   disabled?: boolean
   color?: string
@@ -46,23 +39,37 @@ const emit = defineEmits<{
 }>()
 
 const modelValue = toRef(props, 'modelValue')
-modelValue.value 
 
 const inputComponent = computed(() => {
   switch (props.type) {
-    case 'toggle':    return VToggle
-    case 'checkbox':  return VCheckbox
-    default:          return VRadio
+    case 'checkbox': return VCheckbox
+    case 'toggle':   return VToggle
+    default:         return VRadio
   }
 })
 
-const name = computed(() => props.name || props.label.replace(/\s+/g,'_').toLowerCase())
+const groupName = computed(() =>
+  props.name || props.label.replace(/\s+/g,'_').toLowerCase()
+)
+
+function getProps(opt: Option) {
+  const common = {
+    name:           groupName.value,
+    value:          opt.value,
+    labelText:      opt.label,
+    labelPosition:  props.labelPosition,
+    disabled:       props.disabled,
+    color:          props.color,
+    modelValue:     modelValue.value,
+    'onUpdate:modelValue': (v: any) => emit('update:modelValue', v)
+  }
+  return {
+    ...common,
+    size: 'md'
+  } as any
+}
 </script>
 
 <style scoped>
-fieldset {
-  border: none;
-  padding: 0;
-  margin: 0;
-}
+fieldset { border: none; padding: 0; margin: 0; }
 </style>
