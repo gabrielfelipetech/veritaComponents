586c0a1a82ab0c8a679eb0d8f532a2094eda36dd
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2025-07-28 00:01:12 -0300

    feat: add image module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon', '@nuxt/image',],
   css: ['~/assets/css/tailwind.css'],
   devtools: { enabled: true },
   icon: {
