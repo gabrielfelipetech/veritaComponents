208e768b80b40b05f9cd9f6f9923ec0102539cde
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2025-07-28 00:01:43 -0300

    chore: config build

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -3,11 +3,15 @@ import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [vue(), dts({
+      include: ['components/**/*.{ts,vue}'],
+      exclude: ['**/*.stories.ts', '**/*.spec.ts', '**/__tests__/**']
+    })],
   build: {
     lib: {
       entry: './components/index.ts', 
       name: 'VeritaComponents',
+      
       fileName: (format) => `verita-components.${format}.js`,
     },
     rollupOptions: {
