86016d255cfea1799a7ebfc243df50cdd83db9ba
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2025-08-19 20:12:38 -0300

    fix: adjust type questionNode

--- components/types.ts
+++ components/types.ts
@@ -1,7 +1,30 @@
-export interface NavItem {
+export type NavItem = {
   label: string;
   icon?: string;
   to?: string;
   children?: NavItem[];
   external?: boolean;
-}
+};
+
+export type QuestionNode =
+  | {
+      key: string;
+      label: string;
+      type: 'text' | 'number';
+      placeholder?: string;
+    }
+  | {
+      key: string;
+      label: string;
+      type: 'radio' | 'checkbox' | 'toggle';
+      layout?: 'row' | 'col' | `grid-${1 | 2 | 3 | 4 | 5 | 6}`;
+      childrenLayout?: 'row' | 'col' | `grid-${1 | 2 | 3 | 4 | 5 | 6}`;
+      labelPosition?: 'left' | 'right';
+      options: Array<{
+        label: string;
+        value: any;
+        children?: QuestionNode[];
+        childrenLayout?: 'row' | 'col' | `grid-${1 | 2 | 3 | 4 | 5 | 6}`;
+        allowCustom?: boolean;
+      }>;
+    };
